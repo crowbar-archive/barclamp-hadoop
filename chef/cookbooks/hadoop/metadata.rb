40a6d12f7bea6a59dc944da3b17d8a2f6e000da3
Paul Webster <Paul_Webster@Dell.com>
2011-09-08 04:00:12 -0400

    Initial checking of the Hadoop sqoop barclamp.

--- chef/cookbooks/hadoop/metadata.rb
+++ chef/cookbooks/hadoop/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Dell, Inc."
 maintainer_email "Paul_Webster@Dell.com"
 license          "Apache 2.0 License, Copyright (c) 2011 Dell Inc. - http://www.apache.org/licenses/LICENSE-2.0"
-description      "Installs and configures the hadoop cluster component (HDFS/Map Reduce)"
+description      "The common libraries and utilities that provides the basic Hadoop runtime environment (HDFS/Map Reduce). A set of components and interfaces which implements a distributed filesystem and provides general I/O access for the hadoop framework (serialization, Java RPC and persistent data storage)."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0"
 recipe           "hadoop::default", "Installs hadoop base libraries and configuration."
